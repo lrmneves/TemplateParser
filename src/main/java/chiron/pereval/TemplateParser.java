3488294938ebf963fa6bc1f74699a88705e37b68
Leonardo <lrmneves@poli.ufrj.br>
2014-10-18 11:15:50 -0300

    Fixed the class mapper.

--- src/main/java/chiron/pereval/TemplateParser.java
+++ src/main/java/chiron/pereval/TemplateParser.java
@@ -1,10 +1,11 @@
 package main.java.chiron.pereval;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.sql.*;
+import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,52 +13,42 @@ public class TemplateParser {
     public static final String TIMESTAMP = "<name>Timestamp</name><value>\\d+</value>";
     public static final String UTCTIME = "<name>UTC Time</name>";
     public static final String PID = "<name>pid</name><value>\\d+</value>";
+    public static final String JDBC = "jdbc:postgresql://127.0.0.1:5433/base-scc2";
+    public static final String DB_USER = "postgres";
+    public static final String DB_PASS = "postgres";
+    public static final String TEMPLATE_PATH = "resources/tau-template-supernode.txt";
+    public static final String OUTPUT_PATH = "resources/output-test.txt";
+    public static final String CALL = "1";
+    public static final String SUBCALL = "0";
+    public static HashMap<String,String> methodNameMap = new HashMap<String, String>();
+    public static HashMap<String,String> methodClassMap = new HashMap<String, String>();
+
+
     public static void main(String[] args) {
-        try {
 
-            Class.forName("org.postgresql.Driver");
-
-        } catch (ClassNotFoundException e) {
-
-            System.out.println("Where is your PostgreSQL JDBC Driver? "
-                    + "Include in your library path!");
-            e.printStackTrace();
-            return;
-
-        }
-        System.out.println("PostgreSQL JDBC Driver Registered!");
         Connection connection = null;
-
         try {
-
-            connection = DriverManager.getConnection(
-                    "jdbc:postgresql://127.0.0.1:5433/postgres", "postgres",
-                    "postgres");
-
-        } catch (SQLException e) {
-
-            System.out.println("Connection Failed! Check output console");
+            connection = init();
+            if (connection == null) throw new Exception();
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
-            return;
-
-        }
-
-        if (connection != null) {
-            System.out.println("You made it, take control your database now!");
-        } else {
-            System.out.println("Failed to make connection!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
         try{
-            BufferedReader br = new BufferedReader(new FileReader("resources/tau-template-supernode.txt"));
+            StringBuffer buffer = new StringBuffer();
+            BufferedReader br = new BufferedReader(new FileReader(TEMPLATE_PATH));
 
             String line;
             Pattern timestampPat = Pattern.compile(TIMESTAMP);
             Pattern pidPat = Pattern.compile(PID);
             while ( (line = br.readLine()) != null) {
+                boolean read = false;
                 Matcher timestampMatcher = timestampPat.matcher(line);
                 Matcher pidMatcher = pidPat.matcher(line);
-
                 // check all occurance
                 while (timestampMatcher.find()) {
                     System.out.println(timestampMatcher.group());
@@ -65,11 +56,55 @@ public class TemplateParser {
                 while (pidMatcher.find()) {
                     System.out.println(pidMatcher.group());
                 }
+                if(!read){
+                    buffer.append(line);
+                }
             }
             br.close();
+
+            PreparedStatement pst = null;
+            ResultSet rs = null;
+            pst = connection.prepareStatement("SELECT metric,etime,provfunction FROM eperfeval");
+            rs = pst.executeQuery();
+
+            while (rs.next()) {
+                //"method [{filepath} {,}]" calls subcalls exclusivetime inclusivetime 0 GROUP="groupname"
+                String row = "\""+rs.getString(3) + " [" + methodClassMap.get(rs.getString(1)) + "]\"" + " " + CALL + " " + SUBCALL
+                        +" " +rs.getInt(2) + " " + rs.getInt(2) + " 0 GROUP=\"" + methodNameMap.get(rs.getString(1)) + "\"";
+                buffer.append("\n"+row);
+            }
+            System.out.println(buffer.toString());
+            BufferedWriter bw = new BufferedWriter(new FileWriter(OUTPUT_PATH));
+            bw.append(buffer.toString());
+            bw.flush();
+            bw.close();
         }catch(Exception e){
             e.printStackTrace();
         }
+    }
 
+    private static Connection init()throws ClassNotFoundException,SQLException{
+
+
+        Class.forName("org.postgresql.Driver");
+
+        Connection connection = null;
+        connection = DriverManager.getConnection(
+                JDBC, DB_USER,
+                DB_PASS);
+
+        String prov = "TPROV";
+        String comm = "TCOMM";
+        String comp = "TCOMP";
+
+        methodClassMap.put(prov,"{EProvenance.java}{1,0}");
+        methodClassMap.put(comm,"{EBody.java}{1,0}");
+        methodClassMap.put(comp,"{EHead.java}{1,0}");
+
+        methodNameMap.put(prov,"Provenance");
+        methodNameMap.put(comm,"Communication");
+        methodNameMap.put(comp,"Computation");
+
+        return connection;
     }
 }
